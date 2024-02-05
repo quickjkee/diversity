d28b3472786c11a325ba76f16211de58f779536a
quickjkee <jkee2013@mail.ru>
2024-02-05 11:10:37 +0300

    baseline upd

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from models.baseline_clip import preprocess, ClipBase
 
 
 parser = Parser()
-df = parser.raw_to_df(['files/0_500_pickscore_coco'], do_overlap=True).head(10)
+df = parser.raw_to_df(['files/0_500_pickscore_coco'], do_overlap=True)
 dataset = DiversityDataset(df, preprocess=preprocess)
 
 clip_baseline = ClipBase()
-factor = 'main_object'
+factor = 'angle'
 pred, true = clip_baseline(dataset, factor)
 pred = np.array(pred)
 true = np.array(true)
@@ -24,24 +24,36 @@ prs = []
 rs = []
 f1s = []
 accs = []
+means = []
 threshs = np.linspace(min(pred), max(pred), 10)
 for thresh in threshs:
     curr_pred = np.array(pred) > thresh
     curr_pred = np.array([not item for item in curr_pred]) * 1
     curr_pred = list(curr_pred.astype(int))
+    means.append(np.sum(curr_pred * np.array(true)) / np.linalg.norm(curr_pred * np.array(true)) )
     accs.append(accuracy_score(true, curr_pred))
     f1s.append(f1_score(true, curr_pred))
     rs.append(recall_score(true, curr_pred))
     prs.append(precision_score(true, curr_pred))
 
+stupid = [1] * len(true)
+f1_stupid =f1_score(true, stupid)
+r_stupid = recall_score(true, stupid)
+ac_stupid = accuracy_score(true, stupid)
+pr_stupid = precision_score(true, stupid)
+
+fake_taxi = np.array([not item for item in true]) * 1
+true_mean = np.sum(pred * fake_taxi) / np.linalg.norm(pred * fake_taxi)
 
 import matplotlib.pyplot as plt
-fig, axes = plt.subplots(1, 4, figsize=(20, 5))
-metrics = ['precision', 'recall', 'f1', 'accuracy']
-values = [prs, rs, f1s, accs]
-for j in range(4):
+fig, axes = plt.subplots(1, 5, figsize=(30, 5))
+metrics = ['precision', 'recall', 'f1', 'accuracy', 'means']
+values = [prs, rs, f1s, accs, means]
+stupid = [pr_stupid, r_stupid, f1_stupid, ac_stupid, true_mean]
+for j in range(5):
     axes[j].set_title(metrics[j])
     axes[j].plot(threshs, values[j])
+    axes[j].axhline(stupid[j], color='r', linestyle='-')
 
 plt.suptitle(f'{factor}')
-plt.savefig(f'metrics.png')
+plt.savefig(f'{factor}.png')
